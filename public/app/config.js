10631a13462b5d00497b5a00cffd393958223a55
jobs <hoho88@naver.com>
2026-01-04 10:48:15 +0900

    html

---
+++ public/app/config.js
@@ -0,0 +1,23 @@
+window.APP_CONFIG = {
+  "appName": "우리 아이 유치원 찾기",
+  "nav": [
+    {
+      "key": "home",
+      "label": "홈",
+      "icon": "🏠",
+      "href": "main.html"
+    },
+    {
+      "key": "map",
+      "label": "지도",
+      "icon": "📍",
+      "href": "map.html"
+    },
+    {
+      "key": "favorite",
+      "label": "관심",
+      "icon": "❤️",
+      "href": "favorite.html"
+    }
+  ]
+};
